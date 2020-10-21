5a0232be766f1db74af96d88f87bab8120f5415f
bermmie-work-desktop <bermmie1000@gmail.com>
2020-10-21 15:51:17 +0900

    마무리

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from hone");
+
+const handleProfile = (req, res) => res.send("this is profile");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile)
 
 app.listen(PORT, handleListening);
