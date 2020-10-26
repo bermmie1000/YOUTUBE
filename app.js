18073c98067168d51e2072cf9f12793d8a29d4fc
bermmie-work-desktop <bermmie1000@gmail.com>
2020-10-26 16:26:09 +0900

    2.8

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from hone");
 
@@ -23,5 +20,7 @@ app.use(morgan("dev"));
 
 
 app.get("/", handleHome);
-app.get("/profile", handleProfile)
-app.listen(PORT, handleListening);
+app.get("/profile", handleProfile);
+app.use("/user", userRouter);
+
+export default app;
