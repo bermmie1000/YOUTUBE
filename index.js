538fc2d458b411187aecb76f929aa529d854ef4b
bermmie-work-desktop <bermmie1000@gmail.com>
2020-10-26 15:48:54 +0900

    2.7 Middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -9,16 +14,14 @@ const handleHome = (req, res) => res.send("Hello from hone");
 
 const handleProfile = (req, res) => res.send("this is profile");
 
+/* middleware */
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile)
-
 app.listen(PORT, handleListening);
-
-
-/* 변경이 되어지는지 봅시다. */
-
-/* 상호 작용은 어떻게 돌아가는지 봅시다 ㅎㅎㅎ*/
-
-/* 이제 이 것은 work-desktop BRANCH에서 작성한 것입니다. ㅎㅎ */
-/**이건 어떻게 될까요? */
