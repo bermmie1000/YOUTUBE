685c813be75f145fe2b2c43133afd948611f4990
bermmie-work-desktop <bermmie1000@gmail.com>
2020-10-27 10:40:16 +0900

    2.12

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -17,9 +18,9 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 /** Route */
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 
 export default app;
