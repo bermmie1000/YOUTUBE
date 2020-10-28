00c1115e704f703b1156ce94fd6ea9f28ce02a64
bermmie-work-desktop <bermmie1000@gmail.com>
2020-10-28 11:01:51 +0900

    2.21

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-연습중
+# To do list ⬜✅
+
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Video Detail
+- [x] Edit Video
---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 3333333,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 123213,
+            name: "bermmie",
+            email: "bermmie1000@gmail.com"
+        }
+    },
+    {
+        id: 111111,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 123213,
+            name: "bermmie",
+            email: "bermmie1000@gmail.com"
+        }
+    },
+    {
+        id: 2312312,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 123213,
+            name: "bermmie",
+            email: "bermmie1000@gmail.com"
+        }
+    }
+];
