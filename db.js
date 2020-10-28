9df405a6574305436557d109515808d13ac7740a
bermmie-work-desktop <bermmie1000@gmail.com>
2020-10-28 14:31:04 +0900

    2.22

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: 'Video awesome',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 123213,
             name: "bermmie",
@@ -16,7 +16,7 @@ export const videos = [
         title: 'Video super',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 123213,
             name: "bermmie",
@@ -28,7 +28,7 @@ export const videos = [
         title: 'Video nice',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 123213,
             name: "bermmie",
