de4b5118e3a6a0919e56df09ad03387fd31cc86e
bermmie-work-desktop <bermmie1000@gmail.com>
2020-11-03 17:27:50 +0900

    3.3

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 
 mongoose.connect(
-    "mongodb://localhost:27017/we-tube",
+    process.env.MONGO_URL,
     {
         useNewUrlParser: true,
         useFindAndModify: false
--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
 
