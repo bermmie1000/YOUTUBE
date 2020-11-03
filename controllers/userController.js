ce1072817206ce0215de9f45c125d363581a265d
bermmie-work-desktop <bermmie1000@gmail.com>
2020-11-03 11:44:49 +0900

    2.24

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,10 +18,16 @@ export const postJoin = (req, res) => {
     }
 };
 
-export const login = (req, res) => res.render('login', { pageTitle: "Log In" });
+export const getLogin = (req, res) => res.render('login', { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
+
+
 export const logout = (req, res) => res.render('logout', { pageTitle: "Log Out" });
 
 export const users = (req, res) => res.render('users', { pageTitle: "Users" });
-export const userDetail = (req, res) => res.render('userProfile', { pageTitle: "User Detail" });
+export const userDetail = (req, res) => res.render('userDetail', { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render('editProfile', { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render('changePassword', { pageTitle: "Change Password" });
